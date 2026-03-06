acc2d3162e546f68f3e89795cc9e9c2878e85695
ook-lab <ookubo.yl@gmail.com>
2026-03-07 06:00:29 +0900

    eee

--- scripts/debug/deploy_debug_web.ps1
+++ scripts/debug/deploy_debug_web.ps1
@@ -20,7 +20,7 @@ try {
     # --config で Dockerfile パスを指定、ソース . で .gcloudignore 適用
     gcloud builds submit . `
         --region=$REGION `
-        --config=scripts/debug/cloudbuild.yaml `
+        --config=services/debug-pipeline/cloudbuild.yaml `
         --timeout=1800
 
     if ($LASTEXITCODE -ne 0) {
